28290d179e237e52f0cec1863a1aaccdf7efa1e5
Ville Räsänen <ext-Ville.Rasanen@traficom.fi>
2023-11-01 15:15:34 +0200

    make J & K keymaps silent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,8 +8,8 @@ vim.keymap.set(
   { noremap = true, silent = true, desc = "Resume" }
 )
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
 vim.keymap.set("n", "J", "mzJ`z")
 
