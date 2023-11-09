a55c8338ca02e0fbdec1ac8445d3099ac9b60de4
Ville Räsänen <ext-Ville.Rasanen@traficom.fi>
2023-11-09 12:26:10 +0200

    switch to nvim surround

--- lua/plugins/general/surround.lua
+++ lua/plugins/general/surround.lua
@@ -5,6 +5,17 @@ return {
   },
   {
     "tpope/vim-surround",
-    event = { "BufReadPre", "BufNewFile"},
+    enabled = false,
+    event = { "BufReadPre", "BufNewFile" },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "InsertEnter",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
   },
 }
