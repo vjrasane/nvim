4945850b890949c062aaaa8cc928f0500b4ea229
Ville Räsänen <ext-Ville.Rasanen@traficom.fi>
2023-11-09 12:25:09 +0200

    lazy load flash on buffer open

--- lua/plugins/general/autopairs.lua
+++ lua/plugins/general/autopairs.lua
@@ -1,5 +1,7 @@
 return {
-    'windwp/nvim-autopairs',
+  {
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+    opts = {}, -- this is equalent to setup({}) function
+  },
 }
