8d233817be8d5893647392ca20f6e7e17dd91ee0
Ville Räsänen <ext-Ville.Rasanen@traficom.fi>
2023-11-14 12:21:41 +0200

    fix comments

--- lua/plugins/language/comment.lua
+++ lua/plugins/language/comment.lua
@@ -10,16 +10,29 @@ return {
   },
   {
     "echasnovski/mini.comment",
+    event = { "BufReadPre", "BufNewFile" },
+    enabled = false,
     dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
     },
     opts = {
-      event = { "BufReadPre", "BufNewFile" },
       options = {
         custom_commentstring = function()
           return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
         end,
       },
     },
+  },
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
   },
 }
