60c5dd84a70aec3d73a0ffb90cedaee555b4c990
Ville Räsänen <vjrasane@gmail.com>
2023-12-05 23:25:27 +0200

    shade broken

--- lua/plugins/general/shade.lua
+++ lua/plugins/general/shade.lua
@@ -1,6 +1,7 @@
 return {
   "sunjon/shade.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  enabled = false,
+  -- event = { "BufReadPre", "BufNewFile" },
   opts = {
     overlay_opacity = 45,
     opacity_step = 1,
---
+++ lua/plugins/general/tint.lua
@@ -0,0 +1,9 @@
+return {
+  "levouh/tint.nvim",
+  enabled = false,
+  events = { "BufReadPre", "BufNewFile" },
+  opts = {
+    tint = -41,
+    saturation = 0,
+  },
+}
