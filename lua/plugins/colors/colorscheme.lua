daf2587779b57ebb97648da0bfc9e4a1baf74994
Ville Räsänen <vjrasane@gmail.com>
2023-12-21 18:19:22 +0200

    some more adjustments to kanagawa

--- lua/plugins/colors/colorscheme.lua
+++ lua/plugins/colors/colorscheme.lua
@@ -215,13 +215,18 @@ return {
     config = function()
       require("kanagawa").setup({
         transparent = false,
-        dimInactive = true,
+        dimInactive = false,
         colors = {
           theme = {
             all = {
+
               ui = {
+                float = {
+                  bg = "none",
+                },
                 bg_gutter = "none",
                 bg = "none",
+                bg_dim = "none",
               },
             },
           },
@@ -229,6 +234,20 @@ return {
         overrides = function(colors)
           local theme = colors.theme
           return {
+            -- Floating
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+
+            -- Save an hlgroup with dark background and dimmed foreground
+            -- so that you can use it where your still want darker windows.
+            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+            -- Popular plugins that open floats will link to NormalFloat by default;
+            -- set their background accordingly if you wish to keep them dark and borderless
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
             -- Telescope
             TelescopeTitle = { fg = theme.ui.special, bold = true },
             TelescopePromptNormal = { bg = theme.ui.bg_p1 },
