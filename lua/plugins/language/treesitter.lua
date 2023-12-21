6d5b511c0b3f00d544e42208ba75aab369e303bf
Ville Räsänen <vjrasane@gmail.com>
2023-12-21 18:26:04 +0200

    treesitter context

--- lua/plugins/language/treesitter.lua
+++ lua/plugins/language/treesitter.lua
@@ -2,18 +2,18 @@ return {
   "nvim-treesitter/nvim-treesitter",
   version = false, -- last release is way too old and doesn't work on Windows
   build = ":TSUpdate",
-	-- event = "VeryLazy",
   event = { "BufReadPre", "BufNewFile" },
-  init = function(plugin)
-    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-    -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-    -- during startup.
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-  end,
+  -- init = function(plugin)
+  --   require("lazy.core.loader").add_to_rtp(plugin)
+  --   require("nvim-treesitter.query_predicates")
+  -- end,
   dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      config = function()
+        require("treesitter-context").setup()
+      end,
+    },
     {
       "nvim-treesitter/nvim-treesitter-textobjects",
       config = function()
@@ -71,7 +71,7 @@ return {
       "vim",
       "vimdoc",
       "yaml",
-      "css"
+      "css",
     },
     incremental_selection = {
       enable = true,
@@ -92,8 +92,8 @@ return {
       },
     },
     autotag = {
-      enable = true
-    }
+      enable = true,
+    },
   },
   config = function(_, opts)
     if type(opts.ensure_installed) == "table" then
