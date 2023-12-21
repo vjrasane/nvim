6847f6995eedbd8c311ad4d0c7d20636ce33965a
Ville Räsänen <vjrasane@gmail.com>
2023-12-21 18:13:24 +0200

    disable lspsaga entirely

--- lua/plugins/colors/colorscheme.lua
+++ lua/plugins/colors/colorscheme.lua
@@ -215,9 +215,37 @@ return {
     config = function()
       require("kanagawa").setup({
         transparent = false,
-        dimInactive = false,
+        dimInactive = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+                bg = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            -- Telescope
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            -- Completion
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end,
       })
-      vim.cmd([[colorscheme kanagawa-dragon]])
+      vim.cmd([[colorscheme kanagawa-wave]])
     end,
   },
   {
