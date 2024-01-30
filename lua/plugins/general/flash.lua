c81d288fc797517ccda2ab69025e40a1063ba2ec
Ville Räsänen <vjrasane@gmail.com>
2024-01-30 17:46:00 +0200

    disable flash

--- lua/plugins/general/flash.lua
+++ lua/plugins/general/flash.lua
@@ -1,11 +1,26 @@
 return {
   "folke/flash.nvim",
+  enabled = false,
   event = { "BufReadPre", "BufNewFile" },
   vscode = true,
   opts = {},
   keys = {
-    { "<leader>j", mode = { "n", "v", "x", "o" }, function() require("flash").jump() end, desc = "Flash [J]ump" },
+    {
+      "<leader>j",
+      mode = { "n", "v", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash [J]ump",
+    },
     -- { "<leader>tt", mode = { "n", "v", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash [T]reesitter" },
-    { "<leader>st", mode = { "n", "v", "o", "x" }, function() require("flash").treesitter_search() end, desc = "[T]reesitter Search" },
+    {
+      "<leader>st",
+      mode = { "n", "v", "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "[T]reesitter Search",
+    },
   },
 }
